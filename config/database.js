a74e4c288651571faee5a41bab901b356d90afbe
zaffsa <58152918+zaffsa@users.noreply.github.com>
2024-01-29 16:21:31 -0300

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,15 +1,1 @@
-// config/database.js
-const { Sequelize } = require('sequelize');
-require('dotenv').config();
 
-// Use as credenciais do arquivo config.env
-const sequelize = new Sequelize({
-  host: 'br994.hostgator.com.br',
-  port: 3306,
-  dialect: 'mysql',
-  username: 'ffivep56_samir',
-  password: 'Semprejuntos',
-  database: 'ffivep56_usuarios',  
-});
-
-module.exports = sequelize;
